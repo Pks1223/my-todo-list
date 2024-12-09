c32494bdabe8a6e8ab9c066ec1e12096559edc15
Praween Soni <pramrajkumar1234@gmail.com>
2024-12-09 22:24:14 +0530

    Update button debug

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
 const add = document.getElementById("btn-add");
-add.addEventListener("click", a => {
- let write = document.querySelector("#inputfield input").value;
- if (write.length == "") {
+add.addEventListener("click", (a) => {
+  let write = document.querySelector("#inputfield input").value;
+  if (write.length == "") {
     alert("You not enter any task...!");
- } else{
+  } else {
     item(write);
- }
+  }
 });
 
-function item(task){
- let number = Math.floor(Math.random());
- let id = "task-" + number;
- let html = '<div id="'+ id +'" class="content"><p>' + task + '</p><button id="delete" onclick="deleteitem('+ number +')">Delete</button></div>';
 
- document.querySelector("#listid").innerHTML += html;
+//   let number = Math.floor((Math.random()*100)+1);
+let number = 1;      //  For task Id
+function item(task) {
+  let id = "taskId-" + number;
+  let taskActionBtns =
+    '<div id="' +
+    id +
+    '" class="content"><p>' +
+    task +
+    '</p><div class="actionsBtnBox"><button id="update" onclick="updateitem(' +
+    number + ')">Update</button><button id="delete" onclick="deleteitem(' +
+    number +
+    ')">Completed</button></div></div>';
 
- document.querySelector("#inputfield input").value = "";
+  document.querySelector("#listid").innerHTML += taskActionBtns;
+
+  document.querySelector("#inputfield input").value = "";
+
+  // Update task Id
+  number++;
 }
-
-function deleteitem(number){
- let id = "task-" + number;
- document.getElementById(id).remove();
+function updateitem(taskId){
+   let id = "taskId-" + taskId;
+   console.log(id);
+   // console.log(document.getElementById(id));
+   let idDiv = document.getElementById(id);
+   let idContent = idDiv.querySelector('p');
+   console.log(idContent.textContent);
+}
+function deleteitem(number) {
+  let id = "taskId-" + number;
+  document.getElementById(id).remove();
 }
